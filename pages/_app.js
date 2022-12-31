af7a4256550119c75185879635b4eabd3f0a6ed5
Noah Huffman <77311328+NoahHuffman@users.noreply.github.com>
2022-12-31 15:06:38 -0500

    _app.js device deltion

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,6 @@ import Layout from "../pages/components/Layout";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  <meta name="viewport" content="width=1280, initial-scale=1" />;
-  var siteWidth = 1280;
-  var scale = screen.width / siteWidth;
-
-  document
-    .querySelector('meta[name="viewport"]')
-    .setAttribute(
-      "content",
-      "width=" + siteWidth + ", initial-scale=" + scale + ""
-    );
   return (
     <Layout>
       {/**this line renders the content of the pages*/}
