c7e02011a08f9291c8b3827c23399e29eb1e03b2
Noah Huffman <77311328+NoahHuffman@users.noreply.github.com>
2023-01-03 17:36:24 -0500

    asfsadasdas

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,6 @@ import Layout from "../pages/components/Layout";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  <meta
-    name="viewport"
-    content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
-  />;
   return (
     <Layout>
       {/**this line renders the content of the pages*/}
